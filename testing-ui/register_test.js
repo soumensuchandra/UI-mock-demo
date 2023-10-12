f3cd81491625c7538594ed3c79624a7b2a7f8698
unknown <1bh.soumen@gmail.com>
2023-10-12 18:37:42 +0530

    test suite for email validation

--- testing-ui/register_test.js
+++ testing-ui/register_test.js
@@ -9,7 +9,7 @@ Scenario('Registration Form Success', async ({ I }) => {
     I.fillField('lastName', 'Dow');
     I.wait(2);
     I.see('Email address');
-    I.fillField('email', 'test@test.com');
+    I.fillField('email', 'sachin.t@gmail.com');
     I.wait(2);
     I.click('Continue');
     I.see('Registered Successfully');
@@ -56,8 +56,15 @@ Scenario('Registration Form validation check', async ({ I }) => {
     I.fillField('email', 'test@test.com');
     I.wait(2);
     I.click('Continue');
+    I.see('Email Address doesnot matches with our records');
+    I.wait(2);
+    I.saveScreenshot('validation4.png', true)
+    I.wait(2);
+
+    I.fillField('email', 'ben.stokes@gmail.com');
+    I.click('Continue');
     I.see('Registered Successfully');
     I.wait(2);
-    I.saveScreenshot('validation.png', true)
+    I.saveScreenshot('validation5.png', true)
     //pause();
 });
